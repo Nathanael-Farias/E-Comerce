6eeec50214464a13780c933711c74b92192fce8a
Nathanael-Farias <nathanaelfarias2006@gmail.com>
2025-01-06 19:47:38 -0300

    Update .gitignore and remove unnecessary tracked files

--- API/Program.cs
+++ API/Program.cs
@@ -1,9 +1,14 @@
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<StoreContext>(opt => {
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 
 var app = builder.Build();
