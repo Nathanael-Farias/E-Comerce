0298026294ac742e469293253984722c38a1f7b7
Nathanael-Farias <nathanaelfarias2006@gmail.com>
2025-01-05 23:19:04 -0300

    first commit

---
+++ API/Program.cs
@@ -0,0 +1,17 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+
+var app = builder.Build();
+
+
+
+
+
+app.MapControllers();
+
+app.Run();
