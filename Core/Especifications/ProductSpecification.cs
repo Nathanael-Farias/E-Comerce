39b2b8c2a1c41ab0d96670edefc99d3e919fce2b
Nathanael-Farias <nathanaelfarias2006@gmail.com>
2025-01-10 15:20:08 -0300

    Create ProductSpecification for filtering by brand and type

---
+++ Core/Especifications/ProductSpecification.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core.Entities;
+
+namespace Core.Especifications
+{
+    public class ProductSpecification : BaseSpecification<Product>
+    {
+        
+        public ProductSpecification(string? brand, string? type) : base(x => 
+        (string.IsNullOrWhiteSpace(brand) || x.Brand == brand) && 
+        (string.IsNullOrWhiteSpace(type) || x.Type == type)
+        )
+        {
+            
+        }
+
+    }
+}
