586df15146f46b28f88dcc365dafad502b1a0a3e
Nathanael-Farias <nathanaelfarias2006@gmail.com>
2025-01-10 15:54:14 -0300

    Add ordering logic to SpecificationEvaluator based on OrderBy and OrderByDescending

--- Infrastructure/Data/SpecificationEvaluator.cs
+++ Infrastructure/Data/SpecificationEvaluator.cs
@@ -15,6 +15,15 @@ namespace Infrastructure.Data
             {
                 query = query.Where(spec.Criteria);
             }
+            if(spec.OrderBy != null)
+            {
+                query = query.OrderBy(spec.OrderBy);
+            }
+
+            if(spec.OrderByDescending != null)
+            {
+                query = query.OrderByDescending(spec.OrderByDescending);
+            }
 
             return query;
         }
