93b2f1881e93b306b0c26982c9ec664feb16df15
Nathanael-Farias <nathanaelfarias2006@gmail.com>
2025-01-12 23:37:28 -0300

    Refactor ProductsController to Use BaseApiController for Pagination Logic

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,26 +1,27 @@
+using API.RequestHelpers;
 using Core.Entities;
 using Core.Especifications;
 using Core.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class ProductsController(IGenericRepository<Product> repo ) : ControllerBase
+   
+    public class ProductsController(IGenericRepository<Product> repo ) : BaseApiController
     {
 
 
 
         [HttpGet]
-        public async Task<ActionResult<IReadOnlyList<Product>>> GetProducts(string? brand, 
-            string? type, string? sort)
+        public async Task<ActionResult<IReadOnlyList<Product>>> GetProducts(
+                [FromQuery]ProductSpecParams specParams)
         {
-            var spec = new ProductSpecification(brand, type, sort);
+            var spec = new ProductSpecification(specParams);
 
-            var products = await repo.ListAsync(spec);
+          
 
-            return Ok(products);
+            return await CreatePagedResult(repo, spec, specParams.PageIndex, specParams.PageSize);
         }
 
 
