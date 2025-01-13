103e96ff0c78ebcefb7289cd250286cebd32076d
Nathanael-Farias <nathanaelfarias2006@gmail.com>
2025-01-12 23:35:06 -0300

    Extend Specification Interface with Pagination and Query Filtering

--- Core/Interfaces/ISpecification.cs
+++ Core/Interfaces/ISpecification.cs
@@ -12,6 +12,10 @@ namespace Core.Interfaces
         Expression<Func<G, object>>? OrderBy {get; }
         Expression<Func<G, object>>? OrderByDescending {get; }
         bool IsDistinct {get; }
+        int Take {get; } 
+        int Skip {get;}
+        bool IsPagingEnabled {get;}
+        IQueryable<G> ApplyCriteria(IQueryable<G> query);
     }
 
     public interface ISpecification<G, GResult> : ISpecification<G>
