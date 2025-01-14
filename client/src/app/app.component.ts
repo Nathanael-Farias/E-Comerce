9e0cba936d49e2a237d644522efdefcc23dde5e5
Nathanael-Farias <nathanaelfarias2006@gmail.com>
2025-01-14 07:01:15 -0300

    Add logic to retrieve products in AppComponent

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './layout/header/header.component';
+import { HttpClient } from '@angular/common/http';
+import { Product } from './shared/models/product';
+import { Pagination } from './shared/models/pagination';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  styleUrls: ['./app.component.scss'],
+  standalone: true,
+  imports: [CommonModule, HeaderComponent],
 })
-export class AppComponent {
-  title = ' Cry Tech';
+export class AppComponent implements OnInit {
+  baseUrl = 'https://localhost:5001/api/';
+  private http = inject(HttpClient);
+
+  products: Product[] = [];
+
+  ngOnInit(): void {
+    this.http.get<Pagination<Product>>(this.baseUrl + 'products').subscribe({
+      next: (response) => (this.products = response.data),
+      error: (error) => console.log(error),
+      complete: () => console.log('complete'),
+    });
+  }
 }
