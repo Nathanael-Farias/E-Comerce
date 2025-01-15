ba1597a78735833713ad424ca6636c4088797a1d
Nathanael-Farias <nathanaelfarias2006@gmail.com>
2025-01-15 07:12:24 -0300

    Refactor AppComponent to delegate logic to ShopComponent

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,28 +1,13 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './layout/header/header.component';
-import { HttpClient } from '@angular/common/http';
-import { Product } from './shared/models/product';
-import { Pagination } from './shared/models/pagination';
+import { ShopComponent } from './features/shop/shop.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   standalone: true,
-  imports: [CommonModule, HeaderComponent],
+  imports: [CommonModule, HeaderComponent, ShopComponent],
 })
-export class AppComponent implements OnInit {
-  baseUrl = 'https://localhost:5001/api/';
-  private http = inject(HttpClient);
-
-  products: Product[] = [];
-
-  ngOnInit(): void {
-    this.http.get<Pagination<Product>>(this.baseUrl + 'products').subscribe({
-      next: (response) => (this.products = response.data),
-      error: (error) => console.log(error),
-      complete: () => console.log('complete'),
-    });
-  }
-}
+export class AppComponent {}
