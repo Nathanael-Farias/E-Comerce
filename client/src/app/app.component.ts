715841e149827db9517d7d12584c534dae594785
Nathanael-Farias <nathanaelfarias2006@gmail.com>
2025-01-16 08:56:14 -0300

    Add RouterOutlet to AppComponent for routing support

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './layout/header/header.component';
 import { ShopComponent } from './features/shop/shop.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   standalone: true,
-  imports: [CommonModule, HeaderComponent, ShopComponent],
+  imports: [CommonModule, HeaderComponent, ShopComponent, RouterOutlet],
 })
 export class AppComponent {}
